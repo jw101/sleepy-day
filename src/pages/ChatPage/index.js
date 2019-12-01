197b5e94d4fafeec42cc7cea3f31183e195c1369
jianjunw101 <jianjunw101@gmail.com>
2019-12-01 13:38:41 -0600

    chat

--- src/pages/ChatPage/index.js
+++ src/pages/ChatPage/index.js
@@ -2,13 +2,16 @@
 import React, { Component } from 'react';
 import styles from './chat.module.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import { InputGroup, FormControl, Button, Badge } from 'react-bootstrap';
 
 class Chat extends Component {
     render() {
         return (
             <div className={styles.wrap}>
                 <div className={styles.dialog}>
+                    <h1>
+                        Chatting Room <Badge variant="secondary"></Badge>
+                    </h1>
                     <section className={styles.section}>
                         111
                     </section>
