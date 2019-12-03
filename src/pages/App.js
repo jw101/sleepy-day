6531462999bff37683a3720a0d3f8a0277bd9bbb
John-Zqa <731593917@qq.com>
2019-12-03 00:55:55 -0600

    fix backend-frontend problem

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import styles from './App.module.scss';
-import { Link, Route, withRouter } from 'react-router-dom';
+import {Link, Route, withRouter} from 'react-router-dom';
 import LoginPage from './LoginPage'
 import ChatPage from './ChatPage'
 import RoomPage from './RoomPage'
@@ -8,34 +8,111 @@ import UserPage from './UserPage'
 import AddRoomPage from './AddRoomPage'
 
 class App extends Component {
-  render() {
-    const { location } = this.props;
-    return (
-      <div className={styles.App}>
-        <header className={styles.header}>
-          <div className={styles.box}>
-            <Link to="/"><div className={styles.boxleft}>Chat App</div></Link>
-            {/* <div className={styles.boxright}>
+    constructor(props) {
+        super(props);
+        this.timeout = 250; // Initial timeout duration as a class variable
+
+        this.state = {
+            ws: null,
+            msg: ''
+        };
+    }
+
+    // single websocket instance for the own application and constantly trying to reconnect.
+    componentDidMount() {
+        this.connect();
+    }
+
+    /**
+     * @function connect
+     * This function establishes the connect with the websocket and also ensures constant reconnection if connection closes
+     */
+    connect = () => {
+        let ws = new WebSocket("ws://localhost:4567/chatapp");
+        let that = this; // cache the this
+        let connectInterval;
+
+        // websocket onopen event listener
+        ws.onopen = () => {
+            console.log("connected websocket main component");
+
+            this.setState({ws: ws});
+
+            that.timeout = 250; // reset timer to 250 on open of websocket connection
+            clearTimeout(connectInterval); // clear Interval on on open of websocket connection
+        };
+
+        ws.onmessage = (msg) => {
+            this.check();
+            console.log("get message from backend by websocket");
+
+            this.setState({msg: msg});
+        };
+
+        // websocket onclose event listener
+        ws.onclose = (e) => {
+            console.log(
+                `Socket is closed. Reconnect will be attempted in ${Math.min(
+                    10000 / 1000,
+                    (that.timeout + that.timeout) / 1000
+                )} second.`,
+                e.reason
+            );
+
+            that.timeout = that.timeout + that.timeout; // increment retry interval
+            connectInterval = setTimeout(this.check, Math.min(10000, that.timeout)); // call check function after timeout
+        };
+
+        // websocket onerror event listener
+        ws.onerror = (err) => {
+            console.error(
+                "Socket encountered error: ",
+                err.message,
+                "Closing socket"
+            );
+
+            ws.close();
+        };
+    };
+
+    /**
+     * utilited by the @function connect to check if the connection is close, if so attempts to reconnect
+     */
+    check = () => {
+        const {ws} = this.state;
+        if (!ws || ws.readyState === WebSocket.CLOSED) this.connect(); // check if websocket instance is closed, if so call `connect` function.
+    };
+
+    render() {
+        // const {location} = this.props;
+        return (
+            <div className={styles.App}>
+                <header className={styles.header}>
+                    <div className={styles.box}>
+                        <Link to="/">
+                            <div className={styles.boxleft}>Chat App</div>
+                        </Link>
+                        {/* <div className={styles.boxright}>
               <Link to="/"><p className={location.pathname === "/" ? styles.selected : null}>HOME</p></Link>
               <Link to="/chat"><p className={location.pathname === "/chat" ? styles.selected : null} >CHAT</p></Link>
               <Link to="/room"><p className={location.pathname === "/room" ? styles.selected : null} >ROOM</p></Link>
             </div> */}
-          </div>
-        </header>
-        <section className={styles.content}>
-          <Route path="/" exact component={UserPage} />
-          <Route path="/login" exact component={LoginPage} />
-          <Route path="/chat" exact component={ChatPage} />
-          <Route path="/room" exact component={RoomPage} />
-          <Route path="/addRoom" exact component={AddRoomPage} />
-        </section>
-        <footer className={styles.footer}>
-          <p>© <b>Game Sleepy Day</b></p>
-          <p> _____________________________________________ </p>
-        </footer>
-      </div>
-    );
-  }
+                    </div>
+                </header>
+                <section className={styles.content}>
+                    <Route path="/" exact component={() => <UserPage ws={this.state.ws} msg={this.state.msg}/>}/>
+                    <Route path="/login" exact component={LoginPage}/>
+                    <Route path="/chat" exact component={ChatPage}/>
+                    <Route path="/room" exact component={RoomPage}/>
+                    <Route path="/addRoom" exact component={AddRoomPage}/>
+                </section>
+                <footer className={styles.footer}>
+                    <p>© <b>Game Sleepy Day</b></p>
+                    <p> _____________________________________________ </p>
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default withRouter(App);
--- src/pages/UserPage/index.js
+++ src/pages/UserPage/index.js
@@ -1,18 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import styles from './user.module.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form, Button } from 'react-bootstrap';
-import { Link, Route, withRouter } from 'react-router-dom';
+import {Form, Button} from 'react-bootstrap';
+import {Link, Route, withRouter} from 'react-router-dom';
 
 
 class User extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    // componentDidMount(prevProps, prevState, snapshot) {
+    //     this.sendMessage(JSON.stringify({
+    //         type: "login",
+    //         username: "benjamin1"
+    //     }));
+    // }
+
+    sendMessage = (data) => {
+        const {ws} = this.props; // websocket instance passed as props to the child component.
+        console.log('ws: ' + ws);
+        try {
+            ws.send(data) // send data to the server
+        } catch (error) {
+            console.log("----------error");
+            console.log(error) // catch error
+        }
+        let {msg} = this.props;
+        console.log('msg: ' + msg);
+        console.log('msg json: ' + msg.data);
+    };
+
     render() {
+        this.sendMessage(JSON.stringify({
+            type: "login",
+            username: "benjamin1"
+        }));
+
         return (
             <div className={styles.wrap}>
                 <Form>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>User name</Form.Label>
-                        <Form.Control type="email" placeholder="Enter your user name" />
+                        <Form.Control type="email" placeholder="Enter your user name"/>
                         <Form.Text className="text-muted">
                             Enter your user name to login to your account.
                         </Form.Text>
